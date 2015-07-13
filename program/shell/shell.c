8cde24c517d8849a912fd1c549e19ec4cff14737
Shengwen Cheng <shengwen1997.tw@gmail.com>
2015-07-13 23:35:26 +0800

    Create new shell command "calibrate"

--- program/shell/shell.c
+++ program/shell/shell.c
@@ -10,6 +10,7 @@
 
 /* Shell Command handlers */
 void shell_unknown_cmd(char parameter[][MAX_CMD_LEN], int par_cnt);
+void shell_calibrate(char parameter[][MAX_CMD_LEN], int par_cnt);
 void shell_clear(char parameter[][MAX_CMD_LEN], int par_cnt);
 void shell_help(char parameter[][MAX_CMD_LEN], int par_cnt);
 void shell_license(char parameter[][MAX_CMD_LEN], int par_cnt);
@@ -17,6 +18,7 @@ void shell_license(char parameter[][MAX_CMD_LEN], int par_cnt);
 /* The identifier of the command */
 enum SHELL_CMD_ID {
 	unknown_cmd_ID,
+	calibrate_ID,
 	clear_ID,
 	help_ID,
 	license_ID,
@@ -26,6 +28,7 @@ enum SHELL_CMD_ID {
 //First string don't need to store anything for unknown commands
 command_list shellCmd_list[SHELL_CMD_CNT] = {
 	CMD_DEF(unknown_cmd, shell),
+	CMD_DEF(calibrate, shell),
 	CMD_DEF(clear, shell),
 	CMD_DEF(help, shell),
 	CMD_DEF(license, shell),
@@ -73,6 +76,45 @@ void shell_unknown_cmd(
 	__attribute__((__unused__))int par_cnt)
 {
 	serial1.printf("Command not found\n\r");
+}
+
+enum {ACCEL_CALIBRATE, MAG_CALIBRATE, RC_CALIBRATE};
+
+void shell_calibrate(char parameter[][MAX_CMD_LEN], int par_cnt)
+{
+	int calibrate_mode;
+
+	if(par_cnt != 0) {
+		if(strcmp(parameter[0], "accel") == 0) calibrate_mode = ACCEL_CALIBRATE;
+		else if(strcmp(parameter[0], "mag") == 0) calibrate_mode = MAG_CALIBRATE;
+		else if(strcmp(parameter[0], "rc") == 0) calibrate_mode = RC_CALIBRATE;
+		else calibrate_mode = -1;
+	};
+
+	if(calibrate_mode == -1 || par_cnt == 0) {
+		serial1.printf("type \"calibrate accel\" to calibrate the accelerometer\n\r");
+		serial1.printf("type \"calibrate mag\" to calibrate the magnetometer\n\r");
+		serial1.printf("type \"calibrate rc\" to calibrate the radio controller\n\r");
+		return;
+	}
+
+	uint8_t confirm_result;
+	do {
+		serial1.printf("Are you sure you want do the calibration? (y/n):");
+		confirm_result = serial1.getch();
+		serial1.printf("%c\n\r", confirm_result);
+
+		if(confirm_result == 'n' || confirm_result == 'N') return;
+	} while(confirm_result != 'y' && confirm_result != 'Y');
+
+	switch(calibrate_mode) {
+	    case ACCEL_CALIBRATE:
+		break;
+	    case MAG_CALIBRATE:
+		break;
+	    case RC_CALIBRATE:
+		break;
+	}
 }
 
 void shell_clear(
