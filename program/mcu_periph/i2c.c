e2d58a7cff3ceb29793343f827842612cec6d97b
Shengwen Cheng <shengwen1997.tw@gmail.com>
2015-08-23 13:59:29 +0800

    C99 initialization for i2c

--- program/mcu_periph/i2c.c
+++ program/mcu_periph/i2c.c
@@ -6,21 +6,20 @@
 
 void enable_i2c1()
 {
-	GPIO_InitTypeDef GPIO_InitStruct;
-
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
 
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
-	
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_I2C1);
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource9, GPIO_AF_I2C1);
-	/* SCL = PB8 SDA = PB9 */
 
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_OD;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	/* SCL = PB8, SDA = PB9 */
+	GPIO_InitTypeDef GPIO_InitStruct = {
+		.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9,
+		.GPIO_Mode = GPIO_Mode_AF,
+		.GPIO_Speed = GPIO_Speed_50MHz,
+		.GPIO_OType = GPIO_OType_OD,
+		.GPIO_PuPd = GPIO_PuPd_NOPULL
+	};
 	GPIO_Init(GPIOB, &GPIO_InitStruct);
 	
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
@@ -29,14 +28,14 @@ void enable_i2c1()
 	GPIO_Init(EEPROM_A2_PIN_GROUP, &GPIO_InitStruct);
 	GPIO_SetBits(EEPROM_A2_PIN_GROUP, EEPROM_A2_PIN);
 
-	I2C_InitTypeDef I2C_InitStruct;
-
-	I2C_InitStruct.I2C_ClockSpeed = 400000;
-	I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;
-	I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;
-	I2C_InitStruct.I2C_OwnAddress1 = 0x0A;
-	I2C_InitStruct.I2C_Ack = I2C_Ack_Enable;
-	I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+	I2C_InitTypeDef I2C_InitStruct = {
+		.I2C_ClockSpeed = 400000,
+		.I2C_Mode = I2C_Mode_I2C,
+		.I2C_DutyCycle = I2C_DutyCycle_2,
+		.I2C_OwnAddress1 = 0x0A,
+		.I2C_Ack = I2C_Ack_Enable,
+		.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit
+	};
 	I2C_Init(I2C1, &I2C_InitStruct);
 
 	I2C_AcknowledgeConfig(I2C1,ENABLE);
