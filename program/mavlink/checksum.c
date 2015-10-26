9067b3ffa9dde49eccccd9c8d3c4716cc6adb850
Shengwen Cheng <shengwen1997.tw@gmail.com>
2015-10-26 14:46:29 +0800

    Use global data count as the xor checksum init value

--- program/mavlink/checksum.c
+++ program/mavlink/checksum.c
@@ -1,10 +1,12 @@
 #include <stdint.h>
 
+#include "global.h"
 #include "checksum.h"
 
 uint8_t checksum_generate(uint8_t *data, int data_size)
 {
-	uint8_t result = 0x80; //Just an offset
+	//Se the init value as the global data count
+	uint8_t result = get_global_data_count();
 
 	int i;
 	for(i = 0; i < data_size; i++) {
